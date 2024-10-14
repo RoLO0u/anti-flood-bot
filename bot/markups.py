e9a0519bdbdc876d77a54c3a023154474d5f34ae
RoLO0u <roman.fedunn@gmail.com>
2024-10-14 23:40:20 +0300

    delete shuffling on options

--- bot/markups.py
+++ bot/markups.py
@@ -6,7 +6,6 @@ from bot import const
 
 def captcha_inline() -> InlineKeyboardMarkup:
     random_capts = [(caption[0], caption[1]) for caption in list(const.OPTIONS)]
-    random.shuffle(random_capts)
     listed_markup = [InlineKeyboardButton(text=capt[0], callback_data=f"spam{capt[1]}") for capt in random_capts]
     listed_markup = [listed_markup[:3], listed_markup[3:6], listed_markup[6:]]
     markup = InlineKeyboardMarkup(row_width=3, inline_keyboard=listed_markup)
